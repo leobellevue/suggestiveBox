f696e1e21b40ec135e368a1b3614eda53e6e786a
Leon <h.e.olson@outlook.com>
2022-04-14 17:12:15 -0700

    mongo atlas remote database

--- suggestiveBox.js
+++ suggestiveBox.js
@@ -3,6 +3,16 @@ const app = express();
 const path = require('path');
 const router = express.Router();
 
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "";
+const client = new MongoClient(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  console.log("Connected to database")
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
 router.get('/', function(req, res){
     res.sendFile(path.join(__dirname+'/index.html'));
 });
