e8469149e31c57b665f876d360b6e26acfcdca53
Leon <h.e.olson@outlook.com>
2022-04-15 18:28:11 -0700

    switched to React

--- suggestiveBoxServer.js
+++ suggestiveBoxServer.js
@@ -18,6 +18,6 @@ router.get('/', function(req, res){
 });
 
 app.use('/', router);
-app.listen(process.env.port || 3000);
 
+app.listen(process.env.port || 3000);
 console.log('Running on Port 3000');
